d8ad29241d5a4da5dc678bf6bdee968173789a13
esevillano1 <efsevillano1@gmail.com>
2021-08-11 17:44:31 -0700

    DB-01: Remove the database files from the frontend

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -3,10 +3,13 @@ var MongoClient = require('mongodb').MongoClient;
 // Initial connection will be a localhost connection
 var mongoUri = "mongodb://localhost:27017/testDB";
 
+var collections = ["languages"];
+
 // Connect to the DB
 MongoClient.connect(mongoUri, function(err, db){
     if (!err){
         console.log("Connected to the database");
+        db.close();
     } else { 
         console.log("Failed to connect to the database");
     }
