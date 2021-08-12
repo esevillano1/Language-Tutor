e4a77d61f4e7cf963c6e1416365ef93a6f773c05
esevillano1 <efsevillano1@gmail.com>
2021-08-11 19:58:41 -0700

    DB-02: Add a method for inserting one or many documents

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,15 +1,23 @@
 var MongoClient = require('mongodb').MongoClient;
+var insert = require("./insert");
+
+// Name of DB and Collection
+var dbName = "testDB";
+var collection = "languages";
 
 // Initial connection will be a localhost connection
-var mongoUri = "mongodb://localhost:27017/testDB";
-
-var collections = ["languages"];
+var mongoUri = "mongodb://localhost:27017/" + dbName;
 
 // Connect to the DB
 MongoClient.connect(mongoUri, function(err, db){
     if (!err){
         console.log("Connected to the database");
-        db.close();
+        // If no ID is specified for the _id field, then a unique ID will be assigned for each document
+        var obj = [
+            { language: "English", _id: 1 },
+            { language: "Spanish", _id: 2 }
+        ];
+        insert(db, dbName, collection, obj);
     } else { 
         console.log("Failed to connect to the database");
     }
